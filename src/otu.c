cef480121fd0aace7270283adaf14ba476a1adeb
Julien Stuyck <julien.stuyck@techdrill.com>
2015-09-28 17:22:54 +0100

    OTU is not calculated if Ppo2 is bellow 0.5 bar

--- src/otu.c
+++ src/otu.c
@@ -11,8 +11,9 @@ double otu_const(double time,
 
 {
 	double otu = 0.0;
-
-	otu = time * pow(( 0.5 / (o2_ratio - 0.5)), (-5/6));
+	
+	if (o2_ratio > 0.5)
+		otu = time * pow(( 0.5 / (o2_ratio - 0.5)), (-5/6));
 
 	return (otu);
 }
@@ -23,8 +24,8 @@ double otu_descend(double time,
 {
 	double otu = 0.0;
 
-
-	otu = ((3/11)*time)/(o2_ratio_f-o2_ratio_i)*((pow((o2_ratio_f - 0.5)/0.5),11/6) - pow((o2_ratio_i - 0.5)/0.5, 11/6));
+	if (o2_ratio > 0.5)
+		otu = ((3/11)*time)/(o2_ratio_f-o2_ratio_i)*((pow((o2_ratio_f - 0.5)/0.5),11/6) - pow((o2_ratio_i - 0.5)/0.5, 11/6));
 
 	return otu;
 }
