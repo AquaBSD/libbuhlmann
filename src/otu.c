5a196100cf17c7dcee8b1be9c3155a96e7c8b047
Julien Stuyck <julien.stuyck@techdrill.com>
2015-09-28 15:51:00 +0100

    Add the calculations for the Oxygen Toxicity Unit

---
+++ src/otu.c
@@ -0,0 +1,30 @@
+/*
+	
+	Oxygen Toxicity Unit Calulation
+
+*/
+
+#include <math>
+
+double otu_const(double time,
+                 double o2_ratio)
+
+{
+	double otu = 0.0;
+
+	otu = time * pow(( 0.5 / (o2_ratio - 0.5)), (-5/6));
+
+	return (otu);
+}
+
+double otu_descend(double time,
+                   double o2_ratio_i,
+                   double o2_ratio_f)
+{
+	double otu = 0.0;
+
+
+	otu = ((3/11)*time)/(o2_ratio_f-o2_ratio_i)*((pow((o2_ratio_f - 0.5)/0.5),11/6) - pow((o2_ratio_i - 0.5)/0.5, 11/6));
+
+	return otu;
+}
