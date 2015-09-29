a05cef417d73ceaae9060cdfc28183b26770c533
Julien Stuyck <stuyck@gmail.com>
2015-09-29 11:16:29 +0100

    Correction for compilation

--- src/otu.c
+++ src/otu.c
@@ -4,14 +4,14 @@
 
 */
 
-#include <math>
+#include <math.h>
 
 double otu_const(double time,
                  double o2_ratio)
 
 {
 	double otu = 0.0;
-	
+
 	if (o2_ratio > 0.5)
 		otu = time * pow(( 0.5 / (o2_ratio - 0.5)), (-5/6));
 
