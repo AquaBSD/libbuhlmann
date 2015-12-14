0550e01d27b7f0c2f55f33d56f3c77e22539813f
Julien Stuyck <stuyck@gmail.com>
2015-12-14 12:10:36 +0000

    Add He histogram, pimp the design

--- tools/visoutput.py
+++ tools/visoutput.py
@@ -11,10 +11,12 @@ d_arr = []
 t_arr = []
 
 
-hs =[]
+compN2 = []
+compHe = []
 
 width = 0.35  
 maxpressure = 0.0
+opacity = 0.4
 
 for line in sys.stdin:
 	toks = line.split(" ")
@@ -26,7 +28,14 @@ for line in sys.stdin:
 		histline.append(toks[i]) #1
 		if (float(max(histline)) > maxpressure):
 			maxpressure = float(max(histline))
-	hs.append(histline)
+	compN2.append(histline)
+	histline = []
+	for i in range(2,len(toks)-1,2):
+		histline.append(toks[i]) 
+		if (float(max(histline)) > maxpressure):
+			maxpressure = float(max(histline))
+	compHe.append(histline)
+
 fig = plt.figure()
 ax = fig.add_subplot(121)
 
@@ -37,10 +46,11 @@ plt.gca().invert_yaxis()
 ax2 = fig.add_subplot(122)
 ax2.set_ylim(0.7, maxpressure) 
 
-nbComp = np.arange(len(histline))
+nbComp = np.arange(16)
 
 
-rect = ax2.bar(nbComp,hs[0],width)
+rect = ax2.bar(nbComp,compN2[0],width,alpha=opacity, color='b')
+rect = ax2.bar(nbComp,compHe[0],width +1 ,alpha=opacity, color='r')
 
 
 ax2.set_ylabel('Pressure')
@@ -48,14 +58,19 @@ ax2.set_title('Pressure by compartment')
 #ax2.set_xticks(nbComp + width,1)
 ax2.set_xticklabels(('C01', 'C02', 'C03', 'C04', 'C05','C06','C07','C08','C09','C10','C11','C12','C13','C14','C15','C16'))
 
+plt.xlabel('Compartment')
+plt.ylabel('Pressure (bar)')
 
 axtime = plt.axes([0.2, 0.02, 0.65, 0.03])
-stime= Slider(axtime, 'Time', 0, len(hs)-1, valinit=0,valfmt='%d')
+stime= Slider(axtime, 'Time', 0, len(compN2)-1, valinit=0,valfmt='%d')
 
 def update(val):
 	time = int(stime.val)
 	ax2.clear()
-	rect = ax2.bar(nbComp,hs[time],width)
+	rect = ax2.bar(nbComp,compN2[time],width,alpha=opacity, color='b')
+	ax2.set_xlabel('Compartment')
+	ax2.set_ylabel('Pressure (bar)')
+	ax2.set_xticklabels(('C01', 'C02', 'C03', 'C04', 'C05','C06','C07','C08','C09','C10','C11','C12','C13','C14','C15','C16'))
 	ax2.set_ylim(0.7, maxpressure) 
 	ax.clear()
 	ax.plot(t_arr,d_arr)
