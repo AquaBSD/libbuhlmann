3fe3bd2f230752180f5d98203ac068f0eb7044bf
Julien Stuyck <stuyck@gmail.com>
2015-12-17 11:02:41 +0100

    Add gas mix support, don\'t support gas swtiching yet

--- test/gen_dive.py
+++ test/gen_dive.py
@@ -7,6 +7,8 @@ decrate=20
 sampling=.1
 time=0.0
 depth=0.0
+n2=79.0
+he=0.0
 stime=10.0
 
 def check_negative(value):
@@ -24,31 +26,44 @@ parser.add_argument('-d', '--depth',  type=check_negative, required=True,
 parser.add_argument('-t', '--time', type=check_negative, required=True,
                    dest='time',help='bottom time in minutes.')
 
+parser.add_argument('-g', '--gas', dest='gas', nargs='+',help='gas used for this dive, format : [depth],[n2],[he]; default is air for all the dive. ex: -g 0,66,0 20,')
+
 args = parser.parse_args()
 
 
 btime = float(args.time) 
 maxdepth = float(args.depth)
 
+gas = args.gas
+
+nbgas = len(gas)
+
+if (nbgas == 1):
+	pasedgas = gas[0].split(',')
+	if (len(pasedgas) == 3):
+		n2=float(pasedgas[1])
+		he=float(pasedgas[2])
+
+print gas
 #descent
 while (time < btime and depth < maxdepth):
-	print (str(time) + " " +str(depth/10+1))
+	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), n2, he))
 	depth = depth + (decrate * sampling)
 	time = time + sampling
 depth = float(maxdepth)
-print ("%.2f %.2f" % (time , (depth/10+1)))
+print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), n2, he))
 
 #bottom
 time = time + sampling
 while (time < btime):
-	print ("%.2f %.2f" % (time , (depth/10+1)))
+	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), n2, he))
 	time = time + sampling
 
 #ascent
 depth = depth - (ascrate * sampling)
 time = time + sampling
 while (depth > 0.0):
-	print ("%.2f %.2f" % (time , (depth/10+1)))
+	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), n2, he))
 	depth = depth - (ascrate * sampling)
 	time = time + sampling
 
@@ -56,4 +71,4 @@ depth = 0.0
 while (stime > 0):
 	time = time + sampling
 	stime = stime - sampling
-	print ("%.2f %.2f" % (time , (depth/10+1)))
+	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), n2, he))
