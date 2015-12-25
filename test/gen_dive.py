2b029a5e6b45bd5bbc4f1e7265bf442a92d190b2
Julien Stuyck <stuyck@gmail.com>
2015-12-25 09:13:59 +0100

    Add support of deco gas

--- test/gen_dive.py
+++ test/gen_dive.py
@@ -7,10 +7,10 @@ decrate=20
 sampling=.1
 time=0.0
 depth=0.0
-gas="21,0"
-O2=21.0
+O2=.21
 he=0.0
-stime=10.0
+stime=1.0
+nbgas = 0
 
 def check_negative(value):
     dvalue = float(value)
@@ -27,30 +27,59 @@ parser.add_argument('-d', '--depth',  type=check_negative, required=True,
 parser.add_argument('-t', '--time', type=check_negative, required=True,
                    dest='time',help='bottom time in minutes.')
 
-parser.add_argument('-g', '--gas', dest='gas', nargs='+',help='gas used for this dive, format : [depth],[n2],[he]; default is air for all the dive. ex: -g 0,66,0 20,')
+parser.add_argument('-g', '--gas', dest='gases', nargs='+',help='gas used for this dive, format : [depth],[o2],[he]; default is air for all the dive. ex: -g 90,14,58 for a trimix used at 60m with 14%% O2 and 58%% He')
+
+parser.add_argument('-o', '--deco',dest='decos', nargs='+',help='gas used for decompression, format : [depth],[o2],[he],[time]; ex: -o 20,50,0 for a 50%% nitrox for deco')
 
 args = parser.parse_args()
-
 
 btime = float(args.time) 
 maxdepth = float(args.depth)
 
-gas = args.gas
-if gas:
-	nbgas = len(gas)
+gases = []
+decogasses = []
+
+if args.gases:
+	nbgas = len(args.gases)
+	for gas in args.gases:
+		splitedgas = gas.split(',')
+		if (len(splitedgas) == 3):
+			dict = {}
+			dict['depth'] = float(splitedgas[0])
+			dict['o2'] = float(splitedgas[1])/100
+			dict['he'] = float(splitedgas[2])/100
+			gases.append(dict)
+		else:
+			raise argparse.ArgumentTypeError("%s is an invalid formated gas" % gas)
 else:
-	nbgas = 0
+	dict = {}
+	dict['depth'] = 0.0
+	dict['o2'] = .21
+	dict['he'] = 0.0
+	gases.append(dict)
 
-if (nbgas == 1):
-	pasedgas = gas[0].split(',')
-	if (len(pasedgas) == 3):
-		O2=float(pasedgas[1])
-		he=float(pasedgas[2])
+currgas = gases[0]
 
-print gas
+#deco gas parsing
+
+if args.decos:
+	for gas in args.decos:
+		splitedgas = gas.split(',')
+		if (len(splitedgas) == 4):
+			dict = {}
+			dict['depth'] = float(splitedgas[0])
+			dict['o2'] = float(splitedgas[1])/100
+			dict['he'] = float(splitedgas[2])/100
+			dict['time'] = float(splitedgas[3])
+			decogasses.append(dict)
+		else:
+			raise argparse.ArgumentTypeError("%s is an invalid formated gas" % gas)
+
+
+
 #descent
 while (time < btime and depth < maxdepth):
-	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), O2, he))
+	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), currgas['o2'], currgas['he']))
 	depth = depth + (decrate * sampling)
 	time = time + sampling
 depth = float(maxdepth)
@@ -65,9 +94,24 @@ while (time < btime):
 #ascent
 depth = depth - (ascrate * sampling)
 time = time + sampling
+
+nbdeco = 0
+
+
 while (depth > 0.0):
+	if nbdeco < len(decogasses) and decogasses[nbdeco]['depth'] >= depth:
+		O2 = decogasses[nbdeco]['o2']
+		h2 = decogasses[nbdeco]['he']
+		
+
 	print ("%.2f %.2f %.2f %.2f" % (time , (depth/10+1), O2, he))
-	depth = depth - (ascrate * sampling)
+	if nbdeco < len(decogasses) and decogasses[nbdeco]['depth'] >= depth and decogasses[nbdeco]['time'] > 0:
+		decogasses[nbdeco]['time'] -=  sampling
+		if (decogasses[nbdeco]['time'] < 0):
+			nbdeco += 1
+	else:
+		depth = depth - (ascrate * sampling)
+		
 	time = time + sampling
 
 depth = 0.0
